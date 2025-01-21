556f6c4a0d6eed69c7918d903baf1312c537489c
VadimSvistunov <vadim.svistunov@onilab.com>
2025-01-21 21:05:09 +0300

    add minio to docker compose

--- src/main/java/by/svistunovvv/spring/mypastebin/model/Post.java
+++ src/main/java/by/svistunovvv/spring/mypastebin/model/Post.java
@@ -17,7 +17,49 @@ public class Post {
     @NonNull
     @Column(unique=true)
     private String hash;
+    @NonNull
+    private String url;
     @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "user_id", referencedColumnName = "id")
     private User user;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getHash() {
+        return hash;
+    }
+
+    public void setHash(String hash) {
+        this.hash = hash;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
